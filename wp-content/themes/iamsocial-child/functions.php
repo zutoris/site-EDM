84403e016b06f93f98d8caaf9198e8cd5b2f023f
carl <zutoris1975@yahoo.fr>
2017-03-25 07:39:54 +0100

    import des fichiers depuis le site de l'éditeur

---
+++ wp-content/themes/iamsocial-child/functions.php
@@ -0,0 +1,25 @@
+<?php 
+/**
+ * IAMSocial-child functions and definitions
+ */
+
+
+/** add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}*/
+
+function my_theme_enqueue_styles() {
+
+    $parent_style = 'style.css'; // nom du style déclaré dans iamsocial/fucntions.php
+	
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+?>
